674e4821162776908f4ef73945935b4f6323d67d
jamia2018 <mrreza8757@gmail.com>
2024-02-29 00:25:34 +0530

    learn object in js

---
+++ object.js
@@ -0,0 +1,13 @@
+
+
+let myUser = {
+    yourName: "Akhter",
+    yourEmail: "abc@gmail.com",
+    RollNo: 76676,
+}
+
+myUser.yourEmail = "xyz@gmail.com"
+
+// console.log("My Name is : ",myUser.yourName);
+
+console.log(myUser.yourEmail);
