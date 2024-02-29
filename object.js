71c696293982b32a5679630b83255e0a6ffaeb7b
jamia2018 <mrreza8757@gmail.com>
2024-02-29 22:03:31 +0530

    i taught by M

--- object.js
+++ object.js
@@ -6,8 +6,50 @@ let myUser = {
     RollNo: 76676,
 }
 
-myUser.yourEmail = "xyz@gmail.com"
+// myUser.yourEmail = "xyz@gmail.com"
 
 // console.log("My Name is : ",myUser.yourName);
 
-console.log(myUser.yourEmail);
+// console.log(myUser.yourEmail);
+
+
+// alert("Hii");
+
+
+var name = "jhon";  // before es6
+
+let age = 24;
+const value = 22;
+
+
+let a = 20;  // variable shadoing
+
+{
+   let a = 10; 
+//    console.log(a);
+}
+
+// console.log(a);
+
+let num;  // temporal dead zone
+console.log(num);
+
+
+
+
+/* 
+
+ variable(Hoisting)
+ String **
+ Loop ** 
+ Function **   anonymous , arraow function
+ Object ***
+ Array ** 
+ Higher Order Function (Map, Filter, Reduce) *
+ DOM (Document Object Model) **
+ Callback 
+ Async await 
+ Closure 
+ Promises
+
+*/
